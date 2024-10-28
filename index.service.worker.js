2f1a9af17e148f3341c870e1be1b8a71bdee7067
JillRay973 <143532365+JillRay973@users.noreply.github.com>
2024-10-28 15:13:33 -0400

    Update index.service.worker.js

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1730140384|3656917713';
+const CACHE_VERSION = '1730142709|5981831987';
 /** @type {string} */
 const CACHE_PREFIX = 'iiSXRtemplate-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
